8eddbea604accce3f3dce4d073484f8153e5701a
abhishek18620 <abhishek18620@gmail.com>
2017-03-15 23:08:21 +0530

    first model setup and static files setup

--- src/insideblog/models.py
+++ src/insideblog/models.py
@@ -1,3 +1,4 @@
+import datetime
 from django.db import models
 from django_mysql.models import SizedTextField, ListCharField
 
@@ -13,3 +14,10 @@ class article(models.Model):
         max_length = (6*11)
     )
     pub_date = models.DateTimeField('date published')
+
+    def __str__(self):
+        return self.body
+    
+    # TO CHECK IF ARTICLE PUBLISHED RECENTLY 
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
--- src/insideblog/views.py
+++ src/insideblog/views.py
@@ -1,3 +1,18 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from .models import article
 
 # Create your views here.
+# def home(request):
+#     latest_article_list = article.objects.order_by('-pub_date')[:5]
+#     output = ', '.join([q.body for q in latest_article_list])
+#     return HttpResponse(output)
+
+def home(request):
+    #latest_article_list = article.objects.order_by('-pub_date')[:5]
+    context = locals()
+    return render(request, 'home.html', context)
+
+def about(request):
+    context = locals()
+    return render(request, 'about.html', context)
