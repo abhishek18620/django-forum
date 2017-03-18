7d4354645cdd5b2af5dfe3ee4a07fb6674f94a84
abhishek18620 <abhishek18620@gmail.com>
2017-03-19 05:28:12 +0530

    a bit of change in static files

--- src/blog/settings.py
+++ src/blog/settings.py
@@ -36,6 +36,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'insideblog.apps.InsideblogConfig',
+    'profiles.apps.ProfilesConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
